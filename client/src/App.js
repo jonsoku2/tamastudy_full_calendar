5fa5c500ffe7571cb5d0195d475865486ad1e563
rakutenjongseok <whdtjr2792@gmail.com>
2019-11-04 19:50:07 +0900

    test

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
           <Link to="/otherpage">Other Page</Link>
         </header>
         <h1>Fib calculator</h1>
-        <Route exact path="/" component={Fib} />
+        {/* <Route exact path="/" component={Fib} /> */}
         <Route path="/otherpage" component={OtherPage} />
       </div>
     </Router>
