82f7d37452bcff3f941df55084cdcf508226a4a6
rakutenjongseok <whdtjr2792@gmail.com>
2019-11-04 18:14:26 +0900

    test

--- client/src/Fib.jsx
+++ client/src/Fib.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Fib = props => {
-  return <div>Fib</div>;
+  useEffect(() => {
+    getData();
+  }, []);
+  const [value, setValue] = useState('');
+  const getData = async () => {
+    const response = await axios.get('/');
+    setValue(response.data.string);
+  };
+  return <div>{value} : test 완료</div>;
 };
-
-Fib.propTypes = {};
 
 export default Fib;
